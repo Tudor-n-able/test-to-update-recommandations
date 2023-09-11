e9523c1e4b14e4f678c6adcab39a5c9acad076af
cirlandaniel-nable <138568781+cirlandaniel-nable@users.noreply.github.com>
2023-09-11 11:50:55 +0300

    Update badLogsToUpdate.java

--- badLogsToUpdate.java
+++ badLogsToUpdate.java
@@ -39,7 +39,7 @@ public class Test {
                 .
                 getLogger().info(String.
                         format(
-                                "Log message info: orderstatus={%s}",
+                                "Log message info: orderstatus={%s}, errorcode={%s}, user={%s}, transactionId={%s}", // updated
                                 orderstatus
                         )
                 );
@@ -49,13 +49,13 @@ public class Test {
         LoggerFactory
                 .getLogger(Slf4j.class)
                 .warning(
-                    String.format("Log message warn: orderstatus={%s}, errorcode={%d},  transactionid={%d}",
+                    String.format("Log message warn: orderstatus={%s}, errorcode={%d},  transactionid={%d}, user={%d}", // updated
                             orderstatus,
                             errorcode
                     ));
 
 
-        Logger.getLogger().info(String.format("Log message warn:  errorcode={%d}, user={%s}, transactionid={%d}",
+        Logger.getLogger().info(String.format("Log message warn:  errorcode={%d}, user={%s}, transactionid={%d}, orderstatus={%s}", // updated
                 errorcode,
                 user
         ));
